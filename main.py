0d77ebb50085cbf23dcdf453764a63c7ab36dd32
Keys <49612041+Ceilo@users.noreply.github.com>
2023-06-01 18:45:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import logging
+from logs_module import KEYSACLogs
+
+# Define log files
+KEYSAC_Log_Ban = "ban.log"
+KEYSAC_Log_Error = "error.log"
+KEYSAC_Log_Connect = "connect.log"
+KEYSAC_Log_Disconnect = "disconnect.log"
+KEYSAC_Log_Explosion = "explosion.log"
+
+# Define loggers
+ban_logger = logging.getLogger("ban_logger")
+ban_logger.setLevel(logging.INFO)
+ban_handler = logging.FileHandler(KEYSAC_Log_Ban)
+ban_logger.addHandler(ban_handler)
+
+error_logger = logging.getLogger("error_logger")
+error_logger.setLevel(logging.ERROR)
+error_handler = logging.FileHandler(KEYSAC_Log_Error)
+error_logger.addHandler(error_handler)
+
+connect_logger = logging.getLogger("connect_logger")
+connect_logger.setLevel(logging.INFO)
+connect_handler = logging.FileHandler(KEYSAC_Log_Connect)
+connect_logger.addHandler(connect_handler)
+
+disconnect_logger = logging.getLogger("disconnect_logger")
+disconnect_logger.setLevel(logging.INFO)
+disconnect_handler = logging.FileHandler(KEYSAC_Log_Disconnect)
+disconnect_logger.addHandler(disconnect_handler)
+
+explosion_logger = logging.getLogger("explosion_logger")
+explosion_logger.setLevel(logging.INFO)
+explosion_handler = logging.FileHandler(KEYSAC_Log_Explosion)
+explosion_logger.addHandler(explosion_handler)
+
+# Initialize the KEYSACLogs instance
+logs = KEYSACLogs()
+
+# Define event handlers and actions
+def on_ban(player):
+    ban_logger.info(f"Player {player} was banned.")
+    logs.log_event("Ban", f"Player {player} was banned.")
+    # Add additional actions like notifying other players or taking further steps
+
+def on_error(error_message):
+    error_logger.error(error_message)
+    logs.log_event("Error", error_message)
+    # Add additional error handling logic
+
+def on_connect(player):
+    connect_logger.info(f"Player {player} connected.")
+    logs.log_event("Connect", f"Player {player} connected.")
+    # Add additional actions like verifying player credentials or checking for banned players
+
+def on_disconnect(player):
+    disconnect_logger.info(f"Player {player} disconnected.")
+    logs.log_event("Disconnect", f"Player {player} disconnected.")
+    # Add additional actions like updating player statistics or saving game progress
+
+def on_explosion(player):
+    explosion_logger.info(f"Player {player} caused an explosion.")
+    logs.log_event("Explosion", f"Player {player} caused an explosion.")
+    # Add additional actions like penalizing the player or initiating an investigation
+
+# Example usage
+def main():
+    # Assuming these events are triggered somewhere in your code
+    player = "JohnDoe"
+    error_message = "An error occurred."
+
+    on_ban(player)
+    on_error(error_message)
+    on_connect(player)
+    on_disconnect(player)
+    on_explosion(player)
+
+if __name__ == "__main__":
+    main()
