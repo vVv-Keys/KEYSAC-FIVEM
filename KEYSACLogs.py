0c129cd8902255977f25a8650865fd1aa6579305
Keys <49612041+Ceilo@users.noreply.github.com>
2023-06-01 19:05:04 -0400

    Update KEYSACLogs.py

--- KEYSACLogs.py
+++ KEYSACLogs.py
@@ -1,4 +1,5 @@
 import logging
+from logs_module import KEYSACLogs
 
 # Define log files
 KEYSAC_Log_Ban = "ban.log"
@@ -32,45 +33,6 @@ explosion_logger = logging.getLogger("explosion_logger")
 explosion_logger.setLevel(logging.INFO)
 explosion_handler = logging.FileHandler(KEYSAC_Log_Explosion)
 explosion_logger.addHandler(explosion_handler)
-
-# Define event handlers and actions
-def on_ban(player):
-    ban_logger.info(f"Player {player} was banned.")
-    # Add additional actions like notifying other players or taking further steps
-
-def on_error(error_message):
-    error_logger.error(error_message)
-    # Add additional error handling logic
-
-def on_connect(player):
-    connect_logger.info(f"Player {player} connected.")
-    # Add additional actions like verifying player credentials or checking for banned players
-
-def on_disconnect(player):
-    disconnect_logger.info(f"Player {player} disconnected.")
-    # Add additional actions like updating player statistics or saving game progress
-
-def on_explosion(player):
-    explosion_logger.info(f"Player {player} caused an explosion.")
-    # Add additional actions like penalizing the player or initiating an investigation
-
-# Example usage
-def main():
-    # Assuming these events are triggered somewhere in your code
-    player = "JohnDoe"
-    error_message = "An error occurred."
-
-    on_ban(player)
-    on_error(error_message)
-    on_connect(player)
-    on_disconnect(player)
-    on_explosion(player)
-
-if __name__ == "__main__":
-    main()
-
-import logging
-from logs_module import KEYSACLogs
 
 # Initialize the KEYSACLogs instance
 logs = KEYSACLogs()
@@ -110,4 +72,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
