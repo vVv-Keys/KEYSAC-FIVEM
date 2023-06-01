653d25c24335d3abbe0d4bb4452e2ae92b8b9026
Keys <49612041+Ceilo@users.noreply.github.com>
2023-06-01 18:36:17 -0400

    Update KEYSACLogs.py

--- KEYSACLogs.py
+++ KEYSACLogs.py
@@ -1,40 +1,72 @@
 import logging
 
-class KEYSACLogs:
-    def __init__(self):
-        self.log_files = {
-            "Ban": "ban.log",
-            "Error": "error.log",
-            "Connect": "connect.log",
-            "Disconnect": "disconnect.log",
-            "Explosion": "explosion.log"
-        }
-        
-        self.loggers = {}
-        self.handlers = {}
-        
-        self.setup_loggers()
-        
-    def setup_loggers(self):
-        for log_type, log_file in self.log_files.items():
-            logger = logging.getLogger(f"KEYSAC_{log_type}")
-            logger.setLevel(logging.INFO)
-            
-            handler = logging.FileHandler(log_file)
-            handler.setLevel(logging.INFO)
-            
-            formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-            handler.setFormatter(formatter)
-            
-            logger.addHandler(handler)
-            
-            self.loggers[log_type] = logger
-            self.handlers[log_type] = handler
-    
-    def log_event(self, log_type, message):
-        if log_type in self.loggers:
-            logger = self.loggers[log_type]
-            logger.info(message)
-        else:
-            raise ValueError(f"Invalid log type: {log_type}")
+# Define log files
+KEYSAC_Log_Ban = "ban.log"
+KEYSAC_Log_Error = "error.log"
+KEYSAC_Log_Connect = "connect.log"
+KEYSAC_Log_Disconnect = "disconnect.log"
+KEYSAC_Log_Explosion = "explosion.log"
+
+# Define loggers
+ban_logger = logging.getLogger("ban_logger")
+ban_logger.setLevel(logging.INFO)
+ban_handler = logging.FileHandler(KEYSAC_Log_Ban)
+ban_logger.addHandler(ban_handler)
+
+error_logger = logging.getLogger("error_logger")
+error_logger.setLevel(logging.ERROR)
+error_handler = logging.FileHandler(KEYSAC_Log_Error)
+error_logger.addHandler(error_handler)
+
+connect_logger = logging.getLogger("connect_logger")
+connect_logger.setLevel(logging.INFO)
+connect_handler = logging.FileHandler(KEYSAC_Log_Connect)
+connect_logger.addHandler(connect_handler)
+
+disconnect_logger = logging.getLogger("disconnect_logger")
+disconnect_logger.setLevel(logging.INFO)
+disconnect_handler = logging.FileHandler(KEYSAC_Log_Disconnect)
+disconnect_logger.addHandler(disconnect_handler)
+
+explosion_logger = logging.getLogger("explosion_logger")
+explosion_logger.setLevel(logging.INFO)
+explosion_handler = logging.FileHandler(KEYSAC_Log_Explosion)
+explosion_logger.addHandler(explosion_handler)
+
+# Define event handlers and actions
+def on_ban(player):
+    ban_logger.info(f"Player {player} was banned.")
+    # Add additional actions like notifying other players or taking further steps
+
+def on_error(error_message):
+    error_logger.error(error_message)
+    # Add additional error handling logic
+
+def on_connect(player):
+    connect_logger.info(f"Player {player} connected.")
+    # Add additional actions like verifying player credentials or checking for banned players
+
+def on_disconnect(player):
+    disconnect_logger.info(f"Player {player} disconnected.")
+    # Add additional actions like updating player statistics or saving game progress
+
+def on_explosion(player):
+    explosion_logger.info(f"Player {player} caused an explosion.")
+    # Add additional actions like penalizing the player or initiating an investigation
+
+# Example usage
+def main():
+    # Assuming these events are triggered somewhere in your code
+    player = "JohnDoe"
+    error_message = "An error occurred."
+
+    on_ban(player)
+    on_error(error_message)
+    on_connect(player)
+    on_disconnect(player)
+    on_explosion(player)
+
+if __name__ == "__main__":
+    main()
+
 
