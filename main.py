0182c2e5aa68aa384e97f42e42d37050c0cb4022
Keys <49612041+Ceilo@users.noreply.github.com>
2023-06-01 21:45:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,70 @@
-import logging
 import discord
+import logging
 import asyncio
-
-# Define KEYSACLogs class
-class KEYSACLogs:
-    def __init__(self):
-        self.events = []
-
-    def log_event(self, event_type, event_message):
-        event = {"type": event_type, "message": event_message}
-        self.events.append(event)
-
-    def get_all_events(self):
-        return self.events
-
-# Define log files and loggers
-
-KEYSAC_Log_Ban = "ban.log"
-KEYSAC_Log_Error = "error.log"
-KEYSAC_Log_Connect = "connect.log"
-KEYSAC_Log_Disconnect = "disconnect.log"
-KEYSAC_Log_Explosion = "explosion.log"
-KEYSAC_Log_Audit = "audit.log"  # New audit log file
-
-ban_logger = logging.getLogger("ban_logger")
-ban_logger.setLevel(logging.INFO)
-ban_handler = logging.FileHandler(KEYSAC_Log_Ban)
-ban_logger.addHandler(ban_handler)
-
-error_logger = logging.getLogger("error_logger")
-error_logger.setLevel(logging.ERROR)
-error_handler = logging.FileHandler(KEYSAC_Log_Error)
-error_logger.addHandler(error_handler)
-
-connect_logger = logging.getLogger("connect_logger")
-connect_logger.setLevel(logging.INFO)
-connect_handler = logging.FileHandler(KEYSAC_Log_Connect)
-connect_logger.addHandler(connect_handler)
-
-disconnect_logger = logging.getLogger("disconnect_logger")
-disconnect_logger.setLevel(logging.INFO)
-disconnect_handler = logging.FileHandler(KEYSAC_Log_Disconnect)
-disconnect_logger.addHandler(disconnect_handler)
-
-explosion_logger = logging.getLogger("explosion_logger")
-explosion_logger.setLevel(logging.INFO)
-explosion_handler = logging.FileHandler(KEYSAC_Log_Explosion)
-explosion_logger.addHandler(explosion_handler)
-
-audit_logger = logging.getLogger("audit_logger")  # New audit logger
-audit_logger.setLevel(logging.INFO)  # Set log level as desired
-audit_handler = logging.FileHandler(KEYSAC_Log_Audit)
-audit_logger.addHandler(audit_handler)
-
-# Initialize the KEYSACLogs instance
-logs = KEYSACLogs()
-
-# Define Discord logging handler
-class DiscordHandler(logging.Handler):
-    def __init__(self, client, channel_id):
-        super().__init__()
-        self.client = client
-        self.channel_id = channel_id
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        channel = self.client.get_channel(self.channel_id)
-        if channel:
-            asyncio.create_task(channel.send(log_entry))
 
 # Define event handlers and actions
 
-# ...
+def on_ban(player):
+    try:
+        # Server-side ban check
+        if is_banned(player):
+            return
+
+        ban_logger.info(f"Player {player} was banned.")
+        logs.log_event("Ban", f"Player {player} was banned.")
+        audit_logger.info(f"Ban event - Player: {player}")
+
+        # Add additional actions like notifying other players or taking further steps
+    except Exception as e:
+        error_logger.error(f"An error occurred while handling 'on_ban': {str(e)}")
+
+def on_error(error_message):
+    try:
+        error_logger.error(error_message)
+        logs.log_event("Error", error_message)
+        audit_logger.error(f"Error event - Message: {error_message}")
+
+        # Add additional error handling logic
+    except Exception as e:
+        error_logger.error(f"An error occurred while handling 'on_error': {str(e)}")
+
+def on_connect(player):
+    try:
+        # Server-side connection check
+        if not is_allowed_connection(player):
+            return
+
+        connect_logger.info(f"Player {player} connected.")
+        logs.log_event("Connect", f"Player {player} connected.")
+        audit_logger.info(f"Connect event - Player: {player}")
+
+        # Add additional actions like verifying player credentials or checking for banned players
+    except Exception as e:
+        error_logger.error(f"An error occurred while handling 'on_connect': {str(e)}")
+
+def on_disconnect(player):
+    try:
+        disconnect_logger.info(f"Player {player} disconnected.")
+        logs.log_event("Disconnect", f"Player {player} disconnected.")
+        audit_logger.info(f"Disconnect event - Player: {player}")
+
+        # Add additional actions like updating player statistics or saving game progress
+    except Exception as e:
+        error_logger.error(f"An error occurred while handling 'on_disconnect': {str(e)}")
+
+def on_explosion(player):
+    try:
+        # Server-side explosion check
+        if is_explosion_detected(player):
+            return
+
+        explosion_logger.info(f"Player {player} caused an explosion.")
+        logs.log_event("Explosion", f"Player {player} caused an explosion.")
+        audit_logger.info(f"Explosion event - Player: {player}")
+
+        # Add additional actions like penalizing the player or initiating an investigation
+    except Exception as e:
+        error_logger.error(f"An error occurred while handling 'on_explosion': {str(e)}")
 
 # Example usage
 def main():
@@ -101,7 +94,17 @@ def main():
 
     # Discord bot event handlers
 
-    # ...
+    @client.event
+    async def on_ready():
+        print(f"Logged in as {client.user.name}")
+
+    @client.event
+    async def on_message(message):
+        # Add message handling logic
+
+        # Example of handling a command
+        if message.content.startswith("!hello"):
+            await message.channel.send("Hello!")
 
     try:
         client.run("YOUR_DISCORD_BOT_TOKEN")  # Replace with your Discord bot token
